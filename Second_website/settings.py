22ad42b067b5b647926993b9c094015fa54c941f
YusufKhan2016 <hasanrafsun5@gmail.com>
2023-11-07 17:45:26 +0600

    hay allah

--- Second_website/settings.py
+++ Second_website/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
