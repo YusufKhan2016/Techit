b0c12f67155a4d98752e8b5d9941394f24e5cd41
YusufKhan2016 <hasanrafsun5@gmail.com>
2023-11-07 14:58:08 +0600

    first commit

--- Second_website/urls.py
+++ Second_website/urls.py
@@ -4,9 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, re_path, include
 
+from django.views.static import serve
+from django.conf.urls import url 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('orbitor.urls')),
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':        settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
 urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
