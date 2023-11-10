8dfa3e6546a11e8ae73057ddaedf49107f9e8eac
YusufKhan2016 <hasanrafsun5@gmail.com>
2023-11-11 01:09:23 +0600

    try

--- orbitor/urls.py
+++ orbitor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('/',views.index, name='index'),
+    path('',views.index, name='index'),
     path('about',views.about, name='about'),
     path('blog-sidebar',views.blogsidebar, name='blog-sidebar'),
     path('blog-single/<int:id>',views.blogsingle, name= 'blog-single'),
